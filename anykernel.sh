0e2884793bc60011756586e98246c1570b250f9e
Burhan Siddiquee <burhanverse@gmail.com>
2024-02-13 03:34:23 +0530

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,15 +4,15 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Kernel by Aqua
+kernel.string=A11 kernel by Aqua
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=rosemary
-device.name2=
-device.name3=
+device.name1=lancelot 
+device.name2=lava
+device.name3=shiva
 device.name4=
 device.name5=
 supported.versions=
