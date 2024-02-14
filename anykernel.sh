79691a53d11f4d0ded174352f050d5aa8aef46c1
Burhan Siddiquee <burhanverse@gmail.com>
2024-02-15 03:25:33 +0530

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=A11 kernel by Aqua
+kernel.string=BlackSapphire kernel by Aqua 
 do.devicecheck=1
 do.modules=0
 do.systemless=1
