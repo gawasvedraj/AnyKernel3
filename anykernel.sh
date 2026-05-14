5c1409f6d162db3ed6194d8590bf3fe5727d7ee3
Vedraj Gawas <gawasvedraj@gmail.com>
2026-05-14 21:37:38 +0530

    Protium

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=LineageOS kernel by Its_Vixano @ XDA
+kernel.string=Protium Kernel by Vedraj Gawas
 do.devicecheck=1
 device.name1=moonstone
 device.name2=sunstone
